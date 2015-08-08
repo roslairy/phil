e2dad4fa43b4e9a78ff9fff9f6bbc17b763fa9bb
Roslairy <roslairy@crimro.me>
2015-08-08 11:02:18 +0800

    0.1.35
    修改样式
    取消csrf验证

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,24 +9,24 @@
     @if (!empty($presses->items()[0]->picture))
     style="background-image: url('{{ $presses[0]->picture }}')"
     @endif
-    ><a href="/archive/{{ $presses[0]->id }}" class="sepa1" title="{{ $presses[0]->title }}">{{ $presses[0]->title }}</a></div>
+    ><a href="/archive/{{ $presses[0]->id }}" class="sepa1" title="{{ $presses[0]->title }}"><div class="imgText">{{ $presses[0]->title }}</div></a></div>
     <div class="imgShow newsIntro2"
     @if (!empty($presses->items()[1]->picture))
     style="background-image: url('{{ $presses[1]->picture }}')"
     @endif
-    ><a href="/archive/{{ $presses[1]->id }}" class="newsLink bg" title="{{ $presses[1]->title }}">{{ $presses[1]->title }}</a></div>
+    ><a href="/archive/{{ $presses[1]->id }}" class="newsLink bg" title="{{ $presses[1]->title }}"><div class="imgText">{{ $presses[1]->title }}</div></a></div>
     <div  class="imgShow newsIntro3"
     @if (!empty($presses->items()[2]->picture))
     style="background-image: url('{{ $presses[2]->picture }}')"
     @endif
-    ><a href="/archive/{{ $presses[2]->id }}" class="sepa1" title="{{ $presses[2]->title }}">{{ $presses[2]->title }}</a></div>
+    ><a href="/archive/{{ $presses[2]->id }}" class="sepa1" title="{{ $presses[2]->title }}"><div class="imgText">{{ $presses[2]->title }}</div></a></div>
     <div class="imgShow quote1" >
-    <p style="margin-top:30px;">为天地立心，为生民立命，为往圣继绝学，为万世开太平。
+    <p style="margin-top:40px;">为天地立心，为生民立命，为往圣继绝学，为万世开太平。
         <span style="float: right;magin-left:20px;">——张载</span>
     </p>
     </div>
     <div class="imgShow quote1" >
-        <p style="margin-top:45px;">问，乃思之虔诚。
+        <p style="margin-top:60px;">问，乃思之虔诚。
         <span style="float: right;">——海德格尔</span>
         </p>
     </div>
@@ -34,17 +34,17 @@
     @if (!empty($presses->items()[3]->picture))
     style="background-image: url('{{ $presses[3]->picture }}')"
     @endif
-    ><a href="/archive/{{ $presses[3]->id }}" class="newsLink bg1" title="{{ $presses[3]->title }}">{{ $presses[3]->title }}</a></div>
+    ><a href="/archive/{{ $presses[3]->id }}" class="newsLink bg1" title="{{ $presses[3]->title }}"><div class="imgText">{{ $presses[3]->title }}</div></a></div>
     <div class="imgShow newsIntro5"
     @if (!empty($presses->items()[4]->picture))
     style="background-image: url('{{ $presses[4]->picture }}')"
     @endif
-    ><a href="/archive/{{ $presses[4]->id }}" class="sepa2" title="{{ $presses[4]->title }}">{{ $presses[4]->title }}</a></div>
+    ><a href="/archive/{{ $presses[4]->id }}" class="sepa2" title="{{ $presses[4]->title }}"><div class="imgText">{{ $presses[4]->title }}</div></a></div>
     <div class="imgShow newsIntro6"
     @if (!empty($presses->items()[5]->picture))
     style="background-image: url('{{ $presses[5]->picture }}')"
     @endif
-    ><a href="/archive/{{ $presses[5]->id }}" class="newsLink bg2" title="{{ $presses[5]->title }}">{{ $presses[5]->title }}</a></div>
+    ><a href="/archive/{{ $presses[5]->id }}" class="newsLink bg2" title="{{ $presses[5]->title }}"><div class="imgText">{{ $presses[5]->title }}</div></a></div>
     
     <div class="imgShow quote2" >
     <img src="/img/xiaoxun.png" alt=""></img></div>
